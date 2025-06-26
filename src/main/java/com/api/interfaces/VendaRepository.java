0cc5c9d861b1b91d9cb23a4cdbd9646102158e0b
Max-leal <max13leal@gmail.com>
2025-06-26 16:33:55 -0300

    att

--- src/main/java/com/api/interfaces/VendaRepository.java
+++ src/main/java/com/api/interfaces/VendaRepository.java
@@ -2,8 +2,8 @@ package com.api.interfaces;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.api.models.Pedido;
+import com.api.models.Venda;
 
-public interface VendaRepository extends JpaRepository<Pedido, Long>{
+public interface VendaRepository extends JpaRepository<Venda, Long>{
 
 }
