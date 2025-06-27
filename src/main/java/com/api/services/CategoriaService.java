5414c6abc213b4d52a93ab88c8319390bf5549d4
Max-leal <max13leal@gmail.com>
2025-06-27 14:34:29 -0300

    Implementação de CategoriaService

---
+++ src/main/java/com/api/services/CategoriaService.java
@@ -0,0 +1,42 @@
+package com.api.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.models.Categoria;
+import com.api.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria salvar(Categoria c) {
+		//TODO: depois aplicar validações
+		return repository.save(c);
+	}
+	
+	public List<Categoria> getAll() {
+		return repository.findAll();
+	}
+	
+	public Categoria getCategoriaById(Long id) {
+		return repository.findById(id).orElseThrow(() -> new RuntimeException("Não encontrado"));
+	}
+	
+	public Categoria alterar(Categoria c) {
+		Categoria catSalva = getCategoriaById(c.getId());
+		catSalva.setDescricao(c.getDescricao());
+		
+		return repository.save(catSalva);
+	}
+	
+	public void deletar(Long id) {
+		repository.deleteById(id);
+	}
+	
+}
