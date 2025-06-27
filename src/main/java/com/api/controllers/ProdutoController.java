c94c667b06a2916f9225ae9957d7d3b77bbda690
Max-leal <max13leal@gmail.com>
2025-06-27 14:28:46 -0300

    Implementação de ProdutoController completa

--- src/main/java/com/api/controllers/ProdutoController.java
+++ src/main/java/com/api/controllers/ProdutoController.java
@@ -3,6 +3,7 @@ package com.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.api.models.Contato;
 import com.api.models.Produto;
 import com.api.services.ProdutoService;
 
@@ -48,5 +48,20 @@ public class ProdutoController {
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 		}
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getProdutoById(@PathVariable Long id) {
+		try {
+			return ResponseEntity.ok().body(service.getProdutoById(id));
+
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		service.deletar(id);
 	}
 }
